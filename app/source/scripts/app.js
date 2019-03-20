93484e8c56d929e070a49fb82e053f389e3d74b4
cmcoke <minoru.cm@gmail.com>
2019-03-20 17:48:08 -0500

    used JQuery to show the remainging projects when the view more button is clicked

--- app/source/scripts/app.js
+++ app/source/scripts/app.js
@@ -1,7 +1,6 @@
 // mobile menu
 var navToggle = document.querySelector('.header__menu-icon');
 
-
 navToggle.addEventListener('click', function() {
   document.body.classList.toggle('navigation-is-open');
   navToggle.classList.toggle('header__menu-icon--close-x');
@@ -12,7 +11,6 @@ $(document).ready(function() {
 
   var scrollLink = $('.scroll');
 
-
   scrollLink.click(function(e) {
     e.preventDefault();
     $('body,html').animate({
@@ -20,6 +18,20 @@ $(document).ready(function() {
     }, 1000);
   });
 
+})
 
+//view more button
+$(document).ready(function() {
+
+
+
+  $('.projects__item').slice(0, 0).show();
+  $("#viewMore").on('click', function(e) {
+    e.preventDefault();
+    $('.projects__item:hidden').slice(0, 4).slideDown();
+    if ($('.projects__item:hidden').length == 0) {
+      $("#load").fadeOut('slow');
+    }
+  });
 
 })
