d29fa23494544bbe374eda5161099a05527001f4
cmcoke <minoru.cm@gmail.com>
2019-03-20 12:48:40 -0500

    creating a smooth scrolling effect using JQuery

--- app/source/scripts/app.js
+++ app/source/scripts/app.js
@@ -1,8 +1,25 @@
-// mobile-menu
+// mobile menu
 var navToggle = document.querySelector('.header__menu-icon');
 
 
-navToggle.addEventListener('click', function(){
+navToggle.addEventListener('click', function() {
   document.body.classList.toggle('navigation-is-open');
   navToggle.classList.toggle('header__menu-icon--close-x');
 });
+
+// smooth scrolling
+$(document).ready(function() {
+
+  var scrollLink = $('.scroll');
+
+
+  scrollLink.click(function(e) {
+    e.preventDefault();
+    $('body,html').animate({
+      scrollTop: $(this.hash).offset().top - 100
+    }, 1000);
+  });
+
+
+
+})
